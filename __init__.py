11e686b8c7d56ffced017b79b817d52580cd84dd
Luckiex <std.67122420305@ubru.ac.th>
2026-03-07 17:46:47 +0700

    second commit

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@ from . import data
 
 def create_app():
     app = Flask(__name__)
-
     app.config['SECRET_KEY'] = 'bagstore-secret-2568'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/bagstore_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
